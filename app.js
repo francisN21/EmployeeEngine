3e2268a77130d0497071b42fab9ea54c3cf24691
Francisco Rones <ininorones@gmail.com>
2020-12-29 00:48:39 -0800

    Update: Created starter const for the app

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
+// needed consts for the team 
+const Employee = require("./lib/Employee")
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
+// dependencies
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const util = require("util");
+
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
+// Const to render the HTML file
 const render = require("./lib/htmlRenderer");
-
+// to write the file.
+let writeNewFile = util.promisify(fs.writeFile);
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
